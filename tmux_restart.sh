d7192d00c0fc5b1a864cf5df389e8b0b201e0110
David Torrens <djgtorrens@gmail.com>
2019-12-03 10:02:18 +0000

    Deleted the old version 37 files

--- tmux_restart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/pi/code
-tmux kill-session -t ctrl
-tmux kill-session -t cpu
-tmux new-session -d -s ctrl 'python3 temp_control_037.py'
-tmux new-session -d -s cpu  'python3 cpu_monitor_037.py'
